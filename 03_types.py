888cfd16265b27328bd557d3421ea9bab3791715
Filip Kucharczyk <fku@danskecommodities.com>
2019-11-15 14:35:20 +0100

    meaningful examples

--- 03_types.py
+++ 03_types.py
@@ -24,22 +24,16 @@ order_payed = False
 # - int and int
 # - int and float
 # - str and str
-f = 60
 a = x + 50
-b = x * x # this is multiplication
-c = 20 / x
-
-i = 4.2
-j = 91.001
-d = i + j
+price_in_dkk = 12 * euro_to_dkk # this is multiplication
+dkk_to_euro = 1 / euro_to_dkk
 
 hamlet_beg = "To be or "
 hamlet_all = hamlet_beg + " to code is the question!"
 
-# But we can not add int to str. If you uncomment the line below
-# Python will complain that it does not know how to add a int and
-# str.
-# d = x + hamlet_beg
+# We can not add int to str. If you uncomment the line below Python
+# will complain that it does not know how to add a int and str.
+# d = 40 + "text"
 
 # For this reason we can not have a print statement like below
 # print("the value of x is " + x)
@@ -49,8 +43,9 @@ hamlet_all = hamlet_beg + " to code is the question!"
 # How to convert one type to another?
 
 # Sometimes we will have text containing numbers and would like to
-# add it like a int. Sometimes we want to display value of a int
-# variable like in line 45.
+# add it like a int.
+# Sometimes we want to display value of a int variable like in
+# line 39.
 
 # For this purpose Python offers function converting values to other
 # types
@@ -71,7 +66,7 @@ print("the value of variable g_constant is " + str(g_constant))
 
 # str() for bool
 love_for_python = True
-print("the value of variable g_constant is " + str(love_for_python))
+print("do you already love python?" + str(love_for_python))
 
 # int() and float() to convert from something to int or float
 h = "123"
@@ -83,35 +78,37 @@ p_double = float(p) * 2
 print("Value of p_double: " + str(p_double))
 
 # Note that since you can pass any text to int() and float() it is
-# possible that we accidentally will pass some unconvertable text to
+# possible that we accidentally pass some unconvertable text to
 # int() or float(). If this happens python will "throw an exception",
 # display an error message and stop execution.
 
-# uncomment the following lines to see what happens
+# exercise 1: uncomment the following lines, execute code and see
+# what happens
 # v_desc = "your speed is 50 km/h"
 # v = int(v_desc)
+# Does the error message written by python make any sense to you?
 
-# exercise 1: create variables of all types you have learned so far.
-# Display values of all variables in a print() with some preceeding
-# text message.
+
+# exercise 2: create variables of all types you know so far.
+# Display values of all variables in a print() with preceeding text.
 # Example:
 tz_age = 22
 print("value of tz_age is " + str(tz_age))
 
 
-# exercise 2: create as many different variables as you need and
-# use conversion functions to convert:
+# exercise 3: create variables of different types and use conversion
+# functions to convert:
 # - str to int
 # - int to str
 # - bool to str
 # - str to float
 # - float to str
 # - int to float
-# - float to int, notice what hapens to the fractional part of a
-#   float when it's converted to int
+# - float to int, notice what hapens to the fractional part of
+#   floats during conversion to int
 
 
-# exercise 3: deliberetely try to add str to int, float to str, and
+# exercise 4: deliberetely try to add str to int, float to str, and
 # see what happens when you execute the script. The error message
 # should tell you in which line of code python encountered an issue.
 # There should also be a description of the error. Being able to read
