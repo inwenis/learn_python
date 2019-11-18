691c5abdd59b65219d197cb96da7531a32e6960a
Filip Kucharczyk <fku@danskecommodities.com>
2019-11-18 08:51:33 +0100

    exercises - wip

---
+++ exercises.py
@@ -0,0 +1,39 @@
+# exercise 1: 
+Write a program which given the number N will compute the sum of all
+numbers dividable by 3 and 5 which are less or equal to N.
+N=10 
+1 2 3 4 5 6 7 8 9 10
+3 + 5 + 6 + 9 + 10 = 33
+
+# exercise 2: Factorial
+Write a program which will compute factorial N.
+N factorial denoted as n! is the product of all numbers less than or equal.
+3! = 3x2x1 = 6
+7! = 7*6*5*4*3*2*1 = 5040
+
+# exercise 3: Reversed
+Write a program which will reverse text provided by the user
+czyli "programuje w javie" -> "eivaj w ejumargorp"
+- do it using slicing
+- without slicing
+
+# exercise 4: square
+Write a program which will print a square from astriks.
+   ***
+   * *
+   ***
+Modify the program so that the length will be inputed from console.
+
+# exercise 5: Fibonacci
+Write a program which will print out N elements of the fibonacci sequence.
+For each printed fibonacci number also print out the ratio of the current
+Fibonacci number to the previous.
+What happens when you request a lot of Fibonacci numbers?
+Do you 
+Ile elementów ciągu jesteś w stanie obliczyć używając zmiennych typu int? long? BigInteger?
+
+# exercise
+Create two lists of numbers. Write a programs which will print numbers present in both lists
+- numbers present only in first list
+
+fizz buzz
